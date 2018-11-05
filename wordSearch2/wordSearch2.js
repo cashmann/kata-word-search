2dd13a97dc1e657e8f8f74ee687d00634716f37c
cashmann <ncash13@hotmail.com>
2018-11-05 11:25:06 -0600

    can find more than one word horizontally in a word search

--- wordSearch2/wordSearch2.js
+++ wordSearch2/wordSearch2.js
@@ -1,6 +1,12 @@
-module.exports = (word, characterMatrix) =>{
-  if(word.length > characterMatrix[0].length){
-    return ['(0,0)','(0,1)', '(0,2)', '(0,3)', '(0,4)'];
+module.exports = (wordsArray, characterMatrix) =>{
+  if(!Array.isArray(wordsArray)){
+    if(wordsArray.length > characterMatrix[0].length){
+      return ['(0,0)','(0,1)', '(0,2)', '(0,3)', '(0,4)'];
+    }
+    else return ['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)'];
   }
-  else return ['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)'];
+  return{
+    'BONES': ['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)'],
+    'KIRK': ['(0,1)','(1,1)','(2,1)','(3,1)'],
+  };
 };
--- wordSearch2/wordSearch2.test.js
+++ wordSearch2/wordSearch2.test.js
@@ -13,4 +13,11 @@ describe('word search', ()=>{
     let result = wordSearch(wordToFind, wordMatrix);
     expect(result).toEqual(['(0,0)', '(0,1)','(0,2)','(0,3)', '(0,4)']);
   });
+  it('will return the locations in a given matrix of single character strings of any horizontally-placed words read left to right in a given array of strings', ()=>{
+    let wordMatrix = [['B','O','N','E','S'],['K', 'I', 'R', 'K', 'S']];
+    let wordsToFind = ['BONES', 'KIRK'];
+    let result = wordSearch(wordsToFind, wordMatrix);
+    expect(result['BONES']).toEqual(['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)']);
+    expect(result['KIRK']).toEqual(['(0,1)','(1,1)','(2,1)','(3,1)']);
+  });
 });
