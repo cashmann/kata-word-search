4f1dfe0bd714e4cf82f13920a859634563d946a7
cashmann <ncash13@hotmail.com>
2018-11-08 11:58:47 -0600

    refactoring for more widespread usage: created and tested basic queue

---
+++ wordSearch2/queue.test.js
@@ -0,0 +1,14 @@
+const Queue = require('./queue');
+
+describe('Queue', ()=>{
+  it('can enqueue an item', ()=>{
+    let queue = new Queue();
+    queue.enqueue(10);
+    expect(queue.head).toBe(10);
+  });
+  it('can dequeue items', ()=>{
+    let queue = new Queue();
+    queue.enqueue(10);
+    queue.dequeue();
+  });
+});
