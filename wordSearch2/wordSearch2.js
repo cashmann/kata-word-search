8227bca5134f0bcc7fcf907ee4b2eef328c7b2b4
cashmann <ncash13@hotmail.com>
2018-11-08 12:57:36 -0600

    added a directionCounter to track characters matched per direction

--- wordSearch2/wordSearch2.js
+++ wordSearch2/wordSearch2.js
@@ -1,3 +1,6 @@
+const Heap = require('./heap');
+const Queue = require('./queue');
+
 module.exports = (wordsArray, characterMatrix) =>{
   if(!Array.isArray(wordsArray)){
     if(wordsArray.length > characterMatrix[0].length){
@@ -5,14 +8,20 @@ module.exports = (wordsArray, characterMatrix) =>{
     }
     else return ['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)'];
   }
-  if(wordsArray[0].length > characterMatrix[0].length && wordsArray[1].length> characterMatrix[0].length){
-    return {
-      'BONES': ['(0,0)','(0,1)','(0,2)','(0,3)','(0,4)'],
-      'KIRK': ['(1,0)','(1,1)','(1,2)','(1,3)'],
-    };
-  }
-  return{
-    'BONES': ['(0,0)','(1,0)','(2,0)','(3,0)','(4,0)'],
-    'KIRK': ['(0,1)','(1,1)','(2,1)','(3,1)'],
-  };
+  return coors;
 };
+
+class DirectionCounter{
+  constructor(){
+    this.directions = [
+      {
+        string: 'down',
+        count: '0',
+      },
+      {
+        string: 'right',
+        count: '0',
+      },
+    ];
+  }
+}
